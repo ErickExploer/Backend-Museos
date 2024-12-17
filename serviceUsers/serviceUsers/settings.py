f741efbf1791192ff60d31a025f8646b71f36acf
AbdielDiazPalomino <abdoljdp@gmail.com>
2024-12-17 14:20:16 -0500

    conectando el static React

--- serviceUsers/serviceUsers/settings.py
+++ serviceUsers/serviceUsers/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'serviceUsers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'my-react/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL base para los archivos estáticos
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'my-react', 'build', 'static'),  # Ruta del sistema donde están los estáticos
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
